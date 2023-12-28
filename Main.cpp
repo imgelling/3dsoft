d2df71da05a69b5959d96379faace81e25a4316c
ImGelling <47453119+imgelling@users.noreply.github.com>
2023-12-28 01:33:24 -0500

    code clean, figured the difference between CW and CCW

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,15 @@
-#include <stdint.h>
-#define GAME_SUPPORT_DIRECTX9
 #include "game.h"
 
-struct vertex
+struct Vertex
 {
-	float x, y, z;
-	float r, g, b;
+	float_t x, y, z;
+	float_t r, g, b;
 };
 
-struct triangle
+struct Triangle
 {
-	vertex vertices[3];
+	Vertex vertices[3];
+	float_t averageZ;
 };
 
 class Game : public game::Engine
@@ -18,13 +17,13 @@ class Game : public game::Engine
 
 public:
 	game::PixelMode pixelMode;
-	triangle tri;
+	Triangle tri;
 	game::ThreadPool threadPool;
 	std::atomic<uint64_t> fence;
 
 	Game() : game::Engine()
 	{
-		ZeroMemory(&tri, sizeof(triangle));
+		ZeroMemory(&tri, sizeof(Triangle));
 		fence = 0;
 	}
 
@@ -34,7 +33,6 @@ public:
 
 		attributes.WindowTitle = "Window Title";
 		attributes.VsyncOn = false;
-		attributes.RenderingAPI = game::RenderAPI::DirectX9;
 		geSetAttributes(attributes);
 
 	}
@@ -46,9 +44,10 @@ public:
 			geLogLastError();
 		}
 
+		// Start the threads up
 		threadPool.Start();
 
-
+		// Clockwise vertex winding
 		// top
 		tri.vertices[0].x = 320;
 		tri.vertices[0].y = 130;
@@ -87,13 +86,13 @@ public:
 	}
 
 	void Rotate(float& x, float& y, float theta) {
-		float x_new = (x-320) * cos(theta) - (y-180) * sin(theta);
-		float y_new = (x-320) * sin(theta) + (y - 180) * cos(theta);
+		float_t x_new = (x-320) * cos(theta) - (y-180) * sin(theta);
+		float_t y_new = (x-320) * sin(theta) + (y - 180) * cos(theta);
 		x = x_new + 320;
 		y = y_new + 180;
 	}
 
-	void DrawWireFrame(const triangle &tri, const game::Color &color)
+	void DrawWireFrame(const Triangle &tri, const game::Color &color)
 	{
 		pixelMode.LineClip(
 			(int32_t)tri.vertices[0].x, (int32_t)tri.vertices[0].y,
@@ -110,32 +109,50 @@ public:
 		fence++;
 	}
 
-	inline float_t edgeFunction(const game::Vector2f& a, const game::Vector2f& b, const game::Vector2f& c)
+	// Returns the signed area of the triangle formed by
+	// points a,b,c with clockwise winding
+	inline float_t edgeFunctionCW(const game::Vector2f& a, const game::Vector2f& b, const game::Vector2f& c)
 	{
-		return (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x);
+		return -((c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x));
 	}
 
-	void DrawColored(const triangle& tri)
+	// Returns the signed area of the triangle formed by
+	// points a,b,c with counter (anti) clockwise winding
+	inline float_t edgeFunctionCCW(const game::Vector2f& a, const game::Vector2f& b, const game::Vector2f& c)
 	{
-		int32_t sx1 = (int32_t)tri.vertices[0].x;
-		int32_t sx2 = (int32_t)tri.vertices[1].x;
-		int32_t sx3 = (int32_t)tri.vertices[2].x;
-		int32_t sy1 = (int32_t)tri.vertices[0].y;
-		int32_t sy2 = (int32_t)tri.vertices[1].y;
-		int32_t sy3 = (int32_t)tri.vertices[2].y;
+		return ((c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x));
+	}
 
-		int32_t xmax = sx1 > sx2 ? (sx1 > sx3 ? sx1 : sx3) : (sx2 > sx3 ? sx2 : sx3);
-		int32_t ymax = sy1 > sy2 ? (sy1 > sy3 ? sy1 : sy3) : (sy2 > sy3 ? sy2 : sy3);
-		int32_t xmin = sx1 < sx2 ? (sx1 < sx3 ? sx1 : sx3) : (sx2 < sx3 ? sx2 : sx3);
-		int32_t ymin = sy1 < sy2 ? (sy1 < sy3 ? sy1 : sy3) : (sy2 < sy3 ? sy2 : sy3);
+	inline game::Rectf TriangleBoundingBox(const Triangle& tri)
+	{
+		game::Rectf boundingBox;
 
+		float_t sx1 = tri.vertices[0].x;
+		float_t sx2 = tri.vertices[1].x;
+		float_t sx3 = tri.vertices[2].x;
+		float_t sy1 = tri.vertices[0].y;
+		float_t sy2 = tri.vertices[1].y;
+		float_t sy3 = tri.vertices[2].y;
+
+		boundingBox.right = sx1 > sx2 ? (sx1 > sx3 ? sx1 : sx3) : (sx2 > sx3 ? sx2 : sx3);
+		boundingBox.bottom = sy1 > sy2 ? (sy1 > sy3 ? sy1 : sy3) : (sy2 > sy3 ? sy2 : sy3);
+		boundingBox.x = sx1 < sx2 ? (sx1 < sx3 ? sx1 : sx3) : (sx2 < sx3 ? sx2 : sx3);
+		boundingBox.y = sy1 < sy2 ? (sy1 < sy3 ? sy1 : sy3) : (sy2 < sy3 ? sy2 : sy3);
+
+		return boundingBox;
+	}
+
+	void DrawColored(const Triangle& tri)
+	{
 		game::Vector2f v0(tri.vertices[0].x, tri.vertices[0].y);
 		game::Vector2f v1(tri.vertices[1].x, tri.vertices[1].y);
 		game::Vector2f v2(tri.vertices[2].x, tri.vertices[2].y);
 
-		float_t area = edgeFunction(v0, v1, v2);
+		game::Rectf boundingBox = TriangleBoundingBox(tri);
+
+		float_t area = edgeFunctionCW(v0, v1, v2);
 		float_t oneOverArea = 1.0f / area;
-		float_t w0 = 0.0f;
+		float_t w0(0.0f);
 		float_t w1 = 0.0f;
 		float_t w2 = 0.0f;
 		float_t r = 0.0f;
@@ -144,16 +161,16 @@ public:
 		game::Vector2f p;
 
 
-		for (int32_t j = ymin; j < ymax; ++j) {
-			for (int32_t i = xmin; i < xmax; ++i) {
+		for (int32_t j = (int32_t)boundingBox.y; j < (int32_t)boundingBox.bottom; ++j) {
+			for (int32_t i = (int32_t)boundingBox.x; i < (int32_t)boundingBox.right; ++i) {
 				p = { i + 0.5f, j + 0.5f };
 
-				w0 = edgeFunction(v1, v2, p);
-				if (w0 >= 0) continue;  // >= for clockwise triangles  <= counter
-				w1 = edgeFunction(v2, v0, p);
-				if (w1 >= 0) continue;
-				w2 = edgeFunction(v0, v1, p);
-				if (w2 >= 0) continue;
+				w0 = edgeFunctionCW(v1, v2, p);
+				if (w0 <= 0) continue;  // >= for clockwise triangles  <= counter
+				w1 = edgeFunctionCW(v2, v0, p);
+				if (w1 <= 0) continue;
+				w2 = edgeFunctionCW(v0, v1, p);
+				if (w2 <= 0) continue;
 
 				w0 *= oneOverArea;
 				w1 *= oneOverArea;
@@ -206,7 +223,7 @@ if (overlaps) {
 		geClear(GAME_FRAME_BUFFER_BIT, game::Colors::Blue);
 
 		fence = 0;
-		triangle rotatedTri;
+		Triangle rotatedTri;
 		//ZeroMemory(&rotatedTri, sizeof(triangle));
 		rotatedTri = tri;
 
