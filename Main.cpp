c3e95ff700e30dfc4f9f8feb03121125d139e4b1
ImGelling <47453119+imgelling@users.noreply.github.com>
2024-01-12 17:09:08 -0500

    code clean

--- Main.cpp
+++ Main.cpp
@@ -225,7 +225,7 @@ public:
 			}
 			tris.emplace_back(temp);
 		}
-		my_PerspectiveFOV2(90.0f, 16.0f / 9.0f, 0.1f, 1000.0f, projMat);
+		my_PerspectiveFOV2(90.0f, 16.0f / 9.0f, 0.1f, 100.0f, projMat);
 
 	}
 
@@ -263,101 +263,90 @@ public:
 		}
 	}
 
-	inline game::Triangle RotateTrihZ(const game::Triangle& tri, const float_t theta) const noexcept
+	inline game::Triangle RotateZ(const game::Triangle& tri, const float_t theta) const noexcept
 	{
 		game::Triangle ret(tri);
 		float_t ctheta = cos(theta);
 		float_t stheta = sin(theta);
-		//for (int i = 0; i < 3; i++)
-		{
-			ret.vertices[0].x = (tri.vertices[0].x) * ctheta - (tri.vertices[0].y) * stheta;
-			ret.vertices[0].y = (tri.vertices[0].x) * stheta + (tri.vertices[0].y) * ctheta;
-			ret.vertices[1].x = (tri.vertices[1].x) * ctheta - (tri.vertices[1].y) * stheta;
-			ret.vertices[1].y = (tri.vertices[1].x) * stheta + (tri.vertices[1].y) * ctheta;
-			ret.vertices[2].x = (tri.vertices[2].x) * ctheta - (tri.vertices[2].y) * stheta;
-			ret.vertices[2].y = (tri.vertices[2].x) * stheta + (tri.vertices[2].y) * ctheta;
-		}
+
+		ret.vertices[0].x = (tri.vertices[0].x) * ctheta - (tri.vertices[0].y) * stheta;
+		ret.vertices[0].y = (tri.vertices[0].x) * stheta + (tri.vertices[0].y) * ctheta;
+		ret.vertices[1].x = (tri.vertices[1].x) * ctheta - (tri.vertices[1].y) * stheta;
+		ret.vertices[1].y = (tri.vertices[1].x) * stheta + (tri.vertices[1].y) * ctheta;
+		ret.vertices[2].x = (tri.vertices[2].x) * ctheta - (tri.vertices[2].y) * stheta;
+		ret.vertices[2].y = (tri.vertices[2].x) * stheta + (tri.vertices[2].y) * ctheta;
+
 		return ret;
 	}
 
-	inline game::Triangle RotateTrihX(const game::Triangle& tri, const float_t theta) const noexcept
+	inline game::Triangle RotateX(const game::Triangle& tri, const float_t theta) const noexcept
 	{
 		game::Triangle ret(tri);
 		float_t ctheta = cos(theta);
 		float_t stheta = sin(theta);
-		//for (int i = 0; i < 3; i++)
-		{
-			ret.vertices[0].y = (tri.vertices[0].y) * ctheta - (tri.vertices[0].z) * stheta;
-			ret.vertices[0].z = (tri.vertices[0].y) * stheta + (tri.vertices[0].z) * ctheta;
-			ret.vertices[1].y = (tri.vertices[1].y) * ctheta - (tri.vertices[1].z) * stheta;
-			ret.vertices[1].z = (tri.vertices[1].y) * stheta + (tri.vertices[1].z) * ctheta;
-			ret.vertices[2].y = (tri.vertices[2].y) * ctheta - (tri.vertices[2].z) * stheta;
-			ret.vertices[2].z = (tri.vertices[2].y) * stheta + (tri.vertices[2].z) * ctheta;
-		}
+
+		ret.vertices[0].y = (tri.vertices[0].y) * ctheta - (tri.vertices[0].z) * stheta;
+		ret.vertices[0].z = (tri.vertices[0].y) * stheta + (tri.vertices[0].z) * ctheta;
+		ret.vertices[1].y = (tri.vertices[1].y) * ctheta - (tri.vertices[1].z) * stheta;
+		ret.vertices[1].z = (tri.vertices[1].y) * stheta + (tri.vertices[1].z) * ctheta;
+		ret.vertices[2].y = (tri.vertices[2].y) * ctheta - (tri.vertices[2].z) * stheta;
+		ret.vertices[2].z = (tri.vertices[2].y) * stheta + (tri.vertices[2].z) * ctheta;
+
 		return ret;
 	}
 
-	inline game::Triangle RotateTrihY(const game::Triangle& tri, const float_t theta) const noexcept
+	inline game::Triangle RotateY(const game::Triangle& tri, const float_t theta) const noexcept
 	{
 		game::Triangle ret(tri);
 		float_t ctheta = cos(theta);
 		float_t stheta = sin(theta);
-		//for (int i = 0; i < 3; i++)
-		{
-			ret.vertices[0].x = (tri.vertices[0].x) * ctheta + (tri.vertices[0].z) * stheta;
-			ret.vertices[0].z = (tri.vertices[0].x) * -stheta + (tri.vertices[0].z) * ctheta;
-			ret.vertices[1].x = (tri.vertices[1].x) * ctheta + (tri.vertices[1].z) * stheta;
-			ret.vertices[1].z = (tri.vertices[1].x) * -stheta + (tri.vertices[1].z) * ctheta;
-			ret.vertices[2].x = (tri.vertices[2].x) * ctheta + (tri.vertices[2].z) * stheta;
-			ret.vertices[2].z = (tri.vertices[2].x) * -stheta + (tri.vertices[2].z) * ctheta;
-		}
-		return ret;
-	}
 
-	inline game::Triangle RotateTrihXYZ(const game::Triangle& tri, const float thetaX, const float thetaY, const float thetaZ) const noexcept
-	{
-		game::Triangle ret(tri);
-		ret = RotateTrihX(ret, thetaX);
-		ret = RotateTrihY(ret, thetaY);
-		ret = RotateTrihZ(ret, thetaZ);
+		ret.vertices[0].x = (tri.vertices[0].x) * ctheta + (tri.vertices[0].z) * stheta;
+		ret.vertices[0].z = (tri.vertices[0].x) * -stheta + (tri.vertices[0].z) * ctheta;
+		ret.vertices[1].x = (tri.vertices[1].x) * ctheta + (tri.vertices[1].z) * stheta;
+		ret.vertices[1].z = (tri.vertices[1].x) * -stheta + (tri.vertices[1].z) * ctheta;
+		ret.vertices[2].x = (tri.vertices[2].x) * ctheta + (tri.vertices[2].z) * stheta;
+		ret.vertices[2].z = (tri.vertices[2].x) * -stheta + (tri.vertices[2].z) * ctheta;
 
 		return ret;
 	}
 
-	inline game::Triangle TranslateTri(const game::Triangle& tri, const float_t _x, const float_t _y, const float_t _z) const noexcept
+	inline game::Triangle RotateXYZ(const game::Triangle& tri, const float thetaX, const float thetaY, const float thetaZ) const noexcept
 	{
 		game::Triangle ret(tri);
-		//for (int i = 0; i < 3; i++)
-		{
-			ret.vertices[0].x += _x;
-			ret.vertices[0].y += _y;
-			ret.vertices[0].z += _z;
-			ret.vertices[1].x += _x;
-			ret.vertices[1].y += _y;
-			ret.vertices[1].z += _z;
-			ret.vertices[2].x += _x;
-			ret.vertices[2].y += _y;
-			ret.vertices[2].z += _z;
-		}
+
+		ret = RotateX(ret, thetaX);
+		ret = RotateY(ret, thetaY);
+		ret = RotateZ(ret, thetaZ);
+
 		return ret;
 	}
 
-	inline game::Triangle TranslateTri(const game::Triangle& tri, game::Vector3f& translate) const noexcept
+	inline game::Triangle Translate(const game::Triangle& tri, const float_t _x, const float_t _y, const float_t _z) const noexcept
 	{
 		game::Triangle ret(tri);
-		ret.vertices[0] += translate; //130
+
+		ret.vertices[0].x += _x;
+		ret.vertices[0].y += _y;
+		ret.vertices[0].z += _z;
+		ret.vertices[1].x += _x;
+		ret.vertices[1].y += _y;
+		ret.vertices[1].z += _z;
+		ret.vertices[2].x += _x;
+		ret.vertices[2].y += _y;
+		ret.vertices[2].z += _z;
+
+		return ret;
+	}
+
+	inline game::Triangle Translate(const game::Triangle& tri, game::Vector3f& translate) const noexcept
+	{
+		game::Triangle ret(tri);
+
+		ret.vertices[0] += translate; 
 		ret.vertices[1] += translate;
 		ret.vertices[2] += translate;
-	
-		//ret.vertices[0].x += translate.x; // 122
-		//ret.vertices[0].y += translate.y;
-		//ret.vertices[0].z += translate.z;
-		//ret.vertices[1].x += translate.x;
-		//ret.vertices[1].y += translate.y;
-		//ret.vertices[1].z += translate.z;
-		//ret.vertices[2].x += translate.x;
-		//ret.vertices[2].y += translate.y;
-		//ret.vertices[2].z += translate.z;
+
 		return ret;
 	}
 
@@ -369,7 +358,7 @@ public:
 
 	// gl
 	static void my_PerspectiveFOV(float_t fov, float_t aspect, float_t nearz, float_t farz, float_t* mret) {
-		float_t D2R = 3.14f / 180.0f;
+		float_t D2R = 3.14159f / 180.0f;
 		float_t yScale = 1.0f / tan(D2R * fov / 2);
 		float_t xScale = yScale / aspect;
 		float_t nearmfar = farz - nearz;
@@ -382,8 +371,9 @@ public:
 		memcpy(mret, m, sizeof(float_t) * 16);
 	}
 
+	// left handed
 	static void my_PerspectiveFOV2(const float_t fov, const float_t aspect, const float_t nearz, const float_t farz, float_t (&mret)[16]) {
-		float_t D2R = 3.14f / 180.0f;
+		float_t D2R = 3.14159f / 180.0f;
 		float_t yScale = 1.0f / tan(D2R * fov / 2.0f);
 		float_t xScale = yScale / aspect;
 		float_t nearmfar = farz - nearz;
@@ -393,8 +383,6 @@ public:
 			0,      0,      farz / nearmfar,			 1,
 			0,      0,		-nearz * farz / nearmfar,	 0
 		};
-		//for (int i = 0; i < 16; i++)
-			//mret[i] = m[i];
 		memcpy(&mret, m, sizeof(float_t) * 16);
 	}
 
@@ -500,9 +488,9 @@ public:
 			//test = RotateTrihY(tris[i], rotation);
 			//test = RotateTrihX(test, -rotation);
 			//test = RotateTrihZ(test, rotation * 0.5f);
-			test = RotateTrihXYZ(tris[i], -rotation, rotation, rotation * 0.5f);
+			test = RotateXYZ(tris[i], -rotation, rotation, rotation * 0.5f);
 			//test = TranslateTri(test, 0.0f, 0.0f, 1.5f); // 122
-			test = TranslateTri(test, t);
+			test = Translate(test, t);
 			test = Project(test);
 			quad.emplace_back(test);
 		}
@@ -564,20 +552,18 @@ public:
 		{
 
 			game::Color dColor;
-			float de = 0.0f;
-			float* bpos = software3D.GetDepth();
-			for (int y = 0; y < pixelMode.GetPixelFrameBufferSize().y; y++)
+			float depth = 0.0f;
+			float* zbuffer = software3D.GetDepth();
+			uint32_t* vbuffer = pixelMode.videoBuffer;
+			for (int pos = 0; pos < pixelMode.GetPixelFrameBufferSize().y * pixelMode.GetPixelFrameBufferSize().x; pos++)
 			{
-				for (int x = 0; x < pixelMode.GetPixelFrameBufferSize().x; x++)
-				{
-					de = *bpos;
-					bpos++;
-					de += 1.0f;
-					de = 1.0f / de;
-					dColor.Set(1.0f * de, 1.0f * de, 1.0f * de, 1.0f);
-					pixelMode.Pixel(x, y, dColor);
-				}
-				
+				depth = *zbuffer;
+				zbuffer++;
+				depth += 1.0f;
+				depth = 2.0f / depth;
+				dColor.Set(1.0f * depth, 1.0f * depth, 1.0f * depth, 1.0f);
+				*vbuffer = dColor.packedARGB;
+				vbuffer++;
 			}
 			pixelMode.Text("Showing Depth buffer.", 0, 40, game::Colors::Yellow, 1);
 		}
